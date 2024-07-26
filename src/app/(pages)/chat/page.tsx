df284b52102daa37733078ccbb98eb174bc4e5fe
pankaj <0xpankaj@gmail.com>
2024-07-27 01:58:48 +0545

    chat improvement

--- src/app/(pages)/chat/page.tsx
+++ src/app/(pages)/chat/page.tsx
@@ -97,7 +97,7 @@ const Chat: React.FC = () => {
 
   return (
     <div className="container mx-auto px-4 py-6">
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+      <div className="grid grid-cols-1 justify-items-center md:grid-cols-2 lg:grid-cols-3 gap-4">
         {
           astrologer.map((item, i)=> (
             <AstrologerProfileCard
