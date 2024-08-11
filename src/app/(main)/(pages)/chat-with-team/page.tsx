63748775c5f914e975ee3a3946e8cc355b8f78e2
pankaj <0xpankaj@gmail.com>
2024-08-12 02:08:14 +0545

    chat with team fix

--- src/app/(main)/(pages)/chat-with-team/page.tsx
+++ src/app/(main)/(pages)/chat-with-team/page.tsx
@@ -57,6 +57,7 @@ const ChatWithTeam: React.FC = () => {
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
         {teamMembers.map((member) => (
           <TeamMemberCard
+          onChatClick={()=> console.log("chat clicked")}
             key={member.$id}
             id={member.$id}
             name={member.name}
