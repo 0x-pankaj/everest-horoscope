d88c82b860201dfefce330a4e1879c7d01d176ee
pankaj <0xpankaj@gmail.com>
2024-09-19 02:58:54 +0545

    crouserPicUpdate

--- src/components/HomeCarousel.tsx
+++ src/components/HomeCarousel.tsx
@@ -5,8 +5,8 @@ import Carousel from './Carouse';
 
 
 const carouselImages = [
-    'https://cloud.appwrite.io/v1/storage/buckets/66998177000d52ac33b4/files/66adde7d00388ce0dc6a/view?project=6696bcc3003c30b6b228&mode=admin',
-    'https://cloud.appwrite.io/v1/storage/buckets/66998177000d52ac33b4/files/66addef10033c79f5525/view?project=6696bcc3003c30b6b228&mode=admin'
+    'https://cloud.appwrite.io/v1/storage/buckets/66998177000d52ac33b4/files/66eb4156002f9aab6c92/view?project=6696bcc3003c30b6b228&project=6696bcc3003c30b6b228&mode=admin',
+    'https://cloud.appwrite.io/v1/storage/buckets/66998177000d52ac33b4/files/66eb41e200032c078d68/view?project=6696bcc3003c30b6b228&project=6696bcc3003c30b6b228&mode=admin'
 ];
 
 const HomeCarousel: React.FC = () => {
